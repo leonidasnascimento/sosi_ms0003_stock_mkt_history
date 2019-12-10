90216497efaa4ca0f3b68f755a36a7ad135d9cee
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2019-12-10 12:53:55 -0300

    Adjustments

--- history/routes/index.js
+++ history/routes/index.js
@@ -24,39 +24,17 @@ router.post('/', function (req, res, next) {
     return
   }
 
-  if (req.body.history.length === 0) {
+  if (req.body.history.length > 30) {
     res.status(HttpStatus.LENGTH_REQUIRED).send("Only 30 days of history are allowed")
     return
   }
 
-  new dal().get_history(req.body.code, function (data) {
-    let db_history_length = 0
-
-    if (data) {
-      db_history_length = data.history.length
-
-      //'Desc' sorting 
-      data.history.sort(function (start, end) {
-        return new Date(end.date) - new Date(start.date)
-      })
-    }
-
-    history_index_count = 31 - req.body.history.length
-    history_index_count = history_index_count <= db_history_length ? history_index_count : db_history_length
-
-    for (let index = 0; index < history_index_count; index++) {
-      req.body.history.push(data.history[index])
-    }
-
-    // Adding new history to the database
-    new dal().add_history(req.body, function (data) {
-      res.status(HttpStatus.OK).send(data)
-    }, function (data) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(data)
-    })
+  // Adding new history to the database
+  new dal().add_history(req.body, function (data) {
+    res.status(HttpStatus.OK).send(data)
   }, function (data) {
-    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(data);
-  });
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(data)
+  })
 });
 
 module.exports = router;
