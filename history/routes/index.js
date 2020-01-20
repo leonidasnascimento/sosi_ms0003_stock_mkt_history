2d07306e5a24d0e29151f8565507e759e1e0c0cf
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-01-20 09:41:59 -0300

    Implementation of 'get last price' method

--- history/routes/index.js
+++ history/routes/index.js
@@ -16,6 +16,42 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.get('/last_price', function (req, res, next) {
+  if ((Object.keys(req.query).length === 0) || (Object.keys(req.query).indexOf("code") < 0)) {
+    res.status(HttpStatus.EXPECTATION_FAILED).send("Stock code not informed")
+  }
+
+  new dal().get_history(req.query["code"], function (data) {
+    if (data != null && data.history != null) {
+      var return_data = {
+        code: null,
+        last_price: null
+      }
+
+      //Sorting
+      data.history.sort(function (a, b) {
+        var a_date = a.date.split("/")
+        var b_date = b.date.split("/")
+
+        if (a_date == null || b_date == null || a_date.length < 3 || b_date.length < 3) {
+          return
+        }
+
+        return new Date(Number(b_date[2]), Number(b_date[1]), Number(b_date[0])) - new Date(Number(a_date[2]), Number(a_date[1]), Number(a_date[0]))
+      })
+      
+      return_data.code = data.code
+      return_data.last_price = data.history[0]
+
+      res.status(HttpStatus.OK).send(return_data);
+    } else {
+      res.status(HttpStatus.METHOD_FAILURE).send(data)
+    }
+  }, function (data) {
+    res.status(HttpStatus.METHOD_FAILURE).send(data)
+  });
+});
+
 router.post('/', function (req, res, next) {
   if (!req.body) {
     res.status(HttpStatus.LENGTH_REQUIRED).send("Body message is required")
