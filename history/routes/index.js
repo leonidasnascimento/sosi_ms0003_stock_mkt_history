fcaae40d7f3d417d837d140851bc54cc7a2cc2a5
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-05 11:02:27 -0300

    Implementation of 'stats' endpoint

--- history/routes/index.js
+++ history/routes/index.js
@@ -3,7 +3,53 @@ var router = express.Router();
 var HttpStatus = require('http-status-codes');
 var dal = require("../data/dal_mkt_history");
 
-/* GET home page. */
+/* FUNCTIONS */
+
+//Gest the last price from a given stock history
+var getLastPrice = function (input_json_arr) {
+  var return_data = {}
+
+  if (input_json_arr != null && input_json_arr.history != null) {
+    //Sorting
+    input_json_arr.history.sort(function (a, b) {
+      var a_date = a.date.split("/")
+      var b_date = b.date.split("/")
+
+      if (a_date == null || b_date == null || a_date.length < 3 || b_date.length < 3) {
+        return
+      }
+
+      return new Date(Number(b_date[2]), Number(b_date[1]), Number(b_date[0])) - new Date(Number(a_date[2]), Number(a_date[1]), Number(a_date[0]))
+    })
+
+    return_data = input_json_arr.history[0]
+
+  }
+
+  return return_data
+}
+
+//Gets the avarage result of a given column within a json array
+var getAvarageResult = function (input_json_arr, column_name) {
+  var calc_result = 0.0
+  var found_values_count = 0
+  var values_sum = 0.0
+
+  if ((input_json_arr == null) || (String(column_name) == '')) {
+    return calc_result
+  }
+
+  for (var index = 0; index < input_json_arr.length; index++) {
+    if (column_name in input_json_arr[index]) {
+      found_values_count += 1
+      values_sum += parseFloat(input_json_arr[index][column_name])
+    }
+  }
+
+  calc_result = values_sum / (found_values_count > 0 ? found_values_count : 1)
+  return calc_result
+}
+
 router.get('/', function (req, res, next) {
   if ((Object.keys(req.query).length === 0) || (Object.keys(req.query).indexOf("code") < 0)) {
     res.status(HttpStatus.EXPECTATION_FAILED).send("Stock code not informed")
@@ -16,37 +62,27 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/last_price', function (req, res, next) {
+router.get('/stats', function (req, res, next) {
   if ((Object.keys(req.query).length === 0) || (Object.keys(req.query).indexOf("code") < 0)) {
     res.status(HttpStatus.EXPECTATION_FAILED).send("Stock code not informed")
   }
 
   new dal().get_history(req.query["code"], function (data) {
-    if (data != null && data.history != null) {
-      var return_data = {
-        code: null,
-        last_price: null
+    result_json = {
+      code: req.query["code"],
+      last_price: getLastPrice(data),
+      avarage_values: {
+        days: data.history.length,
+        openning: getAvarageResult(data.history, "openning"),
+        high: getAvarageResult(data.history, "high"),
+        close: getAvarageResult(data.history, "close"),
+        adjusted_close: getAvarageResult(data.history, "adjusted_close"),
+        low: getAvarageResult(data.history, "low"),
+        volume: getAvarageResult(data.history, "volume"),
       }
-
-      //Sorting
-      data.history.sort(function (a, b) {
-        var a_date = a.date.split("/")
-        var b_date = b.date.split("/")
-
-        if (a_date == null || b_date == null || a_date.length < 3 || b_date.length < 3) {
-          return
-        }
-
-        return new Date(Number(b_date[2]), Number(b_date[1]), Number(b_date[0])) - new Date(Number(a_date[2]), Number(a_date[1]), Number(a_date[0]))
-      })
-      
-      return_data.code = data.code
-      return_data.last_price = data.history[0]
-
-      res.status(HttpStatus.OK).send(return_data);
-    } else {
-      res.status(HttpStatus.METHOD_FAILURE).send(data)
     }
+
+    res.status(HttpStatus.OK).send(result_json);
   }, function (data) {
     res.status(HttpStatus.METHOD_FAILURE).send(data)
   });
